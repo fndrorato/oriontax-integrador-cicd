302bc86e37e76b249febd9bee48e84f121a2f71e
Fernando Rorato <fernando.rorato@gmail.com>
2024-07-04 15:24:50 -0300

    Nova validação

--- items/management/commands/run_select.py
+++ items/management/commands/run_select.py
@@ -3,6 +3,7 @@ import sys
 import django
 import psycopg2
 import pandas as pd
+import json
 from psycopg2 import sql
 
 # Get the absolute path to the directory containing this file (run_select.py)
@@ -20,6 +21,9 @@ django.setup()
 
 from django.conf import settings
 from clients.models import Client  # Importe o modelo Client
+from clients.utils import validateSysmo
+from items.models import Item
+
 
 def get_clients():
     return Client.objects.filter(
@@ -32,7 +36,7 @@ def get_clients():
         password_route=''
     )
 
-def connect_and_query(host, user, password, port, database, client_id, client_name):
+def connect_and_query(host, user, password, port, database, client_name):
     try:
         connection = psycopg2.connect(
             user=user,
@@ -59,6 +63,7 @@ def connect_and_query(host, user, password, port, database, client_id, client_na
             
             # Convert rows to a pandas DataFrame
             df = pd.DataFrame(rows, columns=[desc[0] for desc in cursor.description])
+            print(df.info())
             print(f"Número de linhas retornadas: {len(df)}")
             return df            
 
@@ -84,12 +89,32 @@ if __name__ == "__main__":
         password = client.password_route
         port = client.port_route
         database = client.database_route
-        id = client.id
-        name = client.name
+        client_id = client.id
+        client_name = client.name
 
         print(f"Conectando para o cliente {client.name}")
-        connect_and_query(host, user, password, port, database, id, name)
-        df = connect_and_query(host, user, password, port, database, id, name)
-        if df is not None:
-            # Faça algo com o DataFrame, como salvar em um arquivo CSV
+        df_client = connect_and_query(host, user, password, port, database, client_name)
+        if df_client is not None:
             print('Agora necessário carregar os produtos da BASE OrionTax já validados ')        
+            # Pega todos os itens relacionados a esse cliente
+            items_queryset = Item.objects.filter(client=client).values(
+                'code', 'barcode', 'description', 'ncm', 'cest', 'cfop', 'icms_cst', 
+                'icms_aliquota', 'icms_aliquota_reduzida', 'protege', 'cbenef', 
+                'piscofins_cst', 'pis_aliquota', 'cofins_aliquota', 
+                naturezareceita_code=F('naturezareceita__code')
+            )
+            
+            # Converte o queryset em uma lista de dicionários
+            items_list = list(items_queryset.values())
+
+            # Cria um DataFrame a partir da lista de dicionários
+            items_df = pd.DataFrame(items_list)  
+            
+            try:
+                # Chama a função de validação
+                validation_result = validateSysmo(client_id, items_df, df_client)
+                print(json.dumps(validation_result, indent=4))
+                        
+            except Exception as e:  # Catch any unexpected exceptions
+                # self.logger.critical(f"Unexpected error: {e}", exc_info=True)  # Log with traceback
+                print(f"Erro ao validar as comparações do cliente {client_name}: {e}")        
