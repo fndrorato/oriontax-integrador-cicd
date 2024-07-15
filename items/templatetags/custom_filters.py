ecd2fefaed34632e4524cfec69088083fb405781
Fernando Rorato <fernando.rorato@gmail.com>
2024-07-15 18:12:21 -0300

    adicionado aba descricao diferente

--- items/templatetags/custom_filters.py
+++ items/templatetags/custom_filters.py
@@ -8,6 +8,10 @@ def zero_to_empty(value):
     if value == '0':
         return ''
     return value
+    
+@register.filter(name='default_if_none_or_zero')
+def default_if_none_or_zero(value, default=""):
+    return default if value is None or value == 0 else value    
 
 @register.filter(name='int_filter')
 def int_filter(value):
