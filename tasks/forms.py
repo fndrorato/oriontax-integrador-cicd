397ff4c01b098fa5d46e3268c149f0a9aba26d84
Fernando Rorato <fernando.rorato@gmail.com>
2024-07-22 21:35:09 -0300

    atualizando task

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,4 +6,14 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['execution_time', 'description', 'last_execution', 'next_execution']
+        fields = ['execution_time', 'description', 'last_execution', 'next_execution', 'frequency', 'days_of_week']
+
+    frequency = forms.ChoiceField(choices=Task.FREQUENCY_CHOICES, widget=forms.Select())
+    days_of_week = forms.MultipleChoiceField(choices=Task.DAYS_OF_WEEK_CHOICES, widget=forms.CheckboxSelectMultiple(), required=False)
+    
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.days_of_week = ','.join(self.cleaned_data['days_of_week'])
+        if commit:
+            instance.save()
+        return instance    
--- tasks/models.py
+++ tasks/models.py
@@ -2,10 +2,37 @@ from django.db import models
 from auditlog.registry import auditlog
 
 class Task(models.Model):
+    DAILY = 'D'
+    WEEKLY = 'W'
+    FREQUENCY_CHOICES = [
+        (DAILY, 'Diariamente'),
+        (WEEKLY, 'Semanalmente'),
+    ]
+
+    MONDAY = 'MO'
+    TUESDAY = 'TU'
+    WEDNESDAY = 'WE'
+    THURSDAY = 'TH'
+    FRIDAY = 'FR'
+    SATURDAY = 'SA'
+    SUNDAY = 'SU'
+    DAYS_OF_WEEK_CHOICES = [
+        (MONDAY, 'Segunda-Feira'),
+        (TUESDAY, 'Terça-Feira'),
+        (WEDNESDAY, 'Quarta-Feira'),
+        (THURSDAY, 'Quinta-Feira'),
+        (FRIDAY, 'Sexta-Feira'),
+        (SATURDAY, 'Sábado'),
+        (SUNDAY, 'Domingo'),
+    ]
+    
     execution_time = models.TimeField()
     description = models.TextField()
     last_execution = models.DateTimeField(null=True, blank=True)
     next_execution = models.DateTimeField(null=True, blank=True)
+    frequency = models.CharField(max_length=1, choices=FREQUENCY_CHOICES, default=DAILY)
+    days_of_week = models.CharField(max_length=14, blank=True)  # Max length for storing all days
+    
 
     def __str__(self):
         return f"Task: {self.description} scheduled for {self.next_execution}"
